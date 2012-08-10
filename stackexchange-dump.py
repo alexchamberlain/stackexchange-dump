ca658eb4a341fc3dc3a6f069b9783a60b7851c8e
Alex Chamberlain <alex@alexchamberlain.co.uk>
2012-08-10 15:07:15 +0100

    Added a little more information.

--- stackexchange-dump.py
+++ stackexchange-dump.py
@@ -16,8 +16,9 @@ answerer = site.user(answer.owner_id)
 
 print '<h1>%s</h1>' % question.title
 print '<a href="%s">%s</a>' % (question.url, question.url)
-print 'Asked by <a href="%s">%s</a>' % (owner.url, owner.display_name)
+print 'Asked by <a href="%s">%s</a> on StackOverflow.com' % (owner.url, owner.display_name)
 print question.body
 print '<h2>Accepted Answer</h2>'
 print 'Answered by <a href="%s">%s</a>' % (answerer.url, answerer.display_name)
 print answer.body
+print '<p>Reproduced under the Creative Commons Attribution-ShareAlike 3.0 Unported license.</p>'
