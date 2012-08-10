840abafd90836f2ae13211f456b1cd2ab8897b91
Alex Chamberlain <alex@alexchamberlain.co.uk>
2012-08-10 18:55:51 +0100

    Using Stack.py instead, as it supports 2.0

--- stackexchange-dump.py
+++ stackexchange-dump.py
@@ -2,26 +2,26 @@
 # Licensed under MIT license
 
 import sys
-import stackexchange
+from stackpy import Site
 
-site = stackexchange.Site(stackexchange.StackOverflow)
-site.be_inclusive()
+site = Site('stackoverflow')
+#site.be_inclusive()
 
 if len(sys.argv) < 2:
   id = int(raw_input("Enter a question ID: "))
 else:
   id = int(sys.argv[1])
 
-question = site.question(id)
-owner    = site.user(question.owner.id)
-answer   = site.answer(question.accepted_answer_id)
-answerer = site.user(answer.owner_id)
+question = site.questions(id).filter('withbody')[0]
+owner    = site.users(question.owner.user_id)[0]
+answer   = site.answers(question.accepted_answer_id).filter('withbody')[0]
+answerer = site.users(answer.owner.user_id)[0]
 
 print '<h1>%s</h1>' % question.title
-print '<p><a href="%s">%s</a><br />' % (question.url, question.url)
-print 'Asked by <a href="%s">%s</a> on StackOverflow.com</p>' % (owner.url, owner.display_name)
+print '<p><a href="%s">%s</a><br />' % (question.link, question.link)
+print 'Asked by <a href="%s">%s</a> on StackOverflow.com</p>' % (owner.link, owner.display_name)
 print question.body
 print '<h2>Accepted Answer</h2>'
-print 'Answered by <a href="%s">%s</a>' % (answerer.url, answerer.display_name)
+print 'Answered by <a href="%s">%s</a>' % (answerer.link, answerer.display_name)
 print answer.body
 print '<p>Reproduced under the Creative Commons Attribution-ShareAlike 3.0 Unported license.</p>'
