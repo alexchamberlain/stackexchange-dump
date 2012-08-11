4d72753db9fb30f769bab8dcd61f6e415c2f2934
Alex Chamberlain <alex@alexchamberlain.co.uk>
2012-08-11 12:49:56 +0100

    Move to template based generation

--- stackexchange-dump.py
+++ stackexchange-dump.py
@@ -2,26 +2,54 @@
 # Licensed under MIT license
 
 import sys
+import os
+from pprint import pprint
 from stackpy import Site
+from optparse import OptionParser
+from pystache import render
 
-site = Site('stackoverflow')
-#site.be_inclusive()
+if __name__ == "__main__":
+  parser = OptionParser()
+  parser.add_option("-f", "--format", type="string", dest="format", default="html", help="Output format")
+  parser.add_option("-t", "--template_dir", type="string", dest="template_dir", default="./template", help="Template Directory")
+  (options, args) = parser.parse_args()
 
-if len(sys.argv) < 2:
-  id = int(raw_input("Enter a question ID: "))
-else:
-  id = int(sys.argv[1])
+  site = Site('stackoverflow')
+  #site.be_inclusive()
 
-question = site.questions(id).filter('withbody')[0]
-owner    = site.users(question.owner.user_id)[0]
-answer   = site.answers(question.accepted_answer_id).filter('withbody')[0]
-answerer = site.users(answer.owner.user_id)[0]
+  if len(args) == 0:
+    id = int(raw_input("Enter a question ID: "))
+  else:
+    id = int(args[0])
 
-print '<h1>%s</h1>' % question.title
-print '<p><a href="%s">%s</a><br />' % (question.link, question.link)
-print 'Asked by <a href="%s">%s</a> on StackOverflow.com</p>' % (owner.link, owner.display_name)
-print question.body
-print '<h2>Accepted Answer</h2>'
-print 'Answered by <a href="%s">%s</a>' % (answerer.link, answerer.display_name)
-print answer.body
-print '<p>Reproduced under the Creative Commons Attribution-ShareAlike 3.0 Unported license.</p>'
+  question = site.questions(id).filter('withbody')[0]
+  owner    = site.users(question.owner.user_id)[0]
+  answer   = site.answers(question.accepted_answer_id).filter('withbody')[0]
+  answerer = site.users(answer.owner.user_id)[0]
+
+  data = {
+    'question': {
+      'title': question.title,
+      'link': question.link,
+      'body': question.body,
+      'owner': {
+        'link': owner.link,
+        'display_name': owner.display_name
+      }
+    },
+    'answer': {
+      'body': answer.body,
+      'owner': {
+        'link': answerer.link,
+	'display_name': answerer.display_name
+      }
+    }
+  }
+
+  options.template     = ''.join([options.template_dir, '/', options.format])
+
+  if os.path.isfile(options.template):
+    t = open(options.template).read()
+    print render(t, data).encode('utf-8')
+  else:
+    print 'Invalid format option'
